d97e9db602f32d1d7f393c483ce16447b404cbc1
eden-kl <wl02635565@gmail.com>
2024-12-19 21:32:29 +0800

    [add] 新增account get list route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function () {
+    Route::get('/', 'AccountController@get');
+});
