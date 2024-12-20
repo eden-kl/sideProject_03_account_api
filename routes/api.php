da7918dbe6881d569f242431489002f9ab22efaf
eden-kl <wl02635565@gmail.com>
2024-12-20 20:48:48 +0800

    [add] 新增allow ip檢查

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\Api\v1\AccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function () {
-    Route::get('account', [AccountController::class, 'getList']);
+    Route::get('account', [AccountController::class, 'getList'])->middleware('ipCheck')->name('api.v1.account.list');
 });
