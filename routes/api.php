cc71fa5c74da21cc654f2afd5873d0ac1038bc0d
eden-kl <wl02635565@gmail.com>
2024-12-25 16:20:43 +0800

    [add] 新增create account的route

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function () {
     Route::get('account', [AccountController::class, 'getList'])->middleware('ipCheck')->name('api.v1.account.list');
+    Route::post('account', [AccountController::class, 'createAccount'])->middleware('ipCheck')->name('api.v1.account.create');
 });
