8005ade359187c0956625d4f36ff6d785be8dab2
eden-kl <wl02635565@gmail.com>
2024-12-27 20:17:59 +0800

    [add] 新增更新帳號api

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,5 @@ Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function () {
     Route::get('accounts', [AccountController::class, 'getList'])->middleware('ipCheck')->name('api.v1.accounts.list');
     Route::post('accounts', [AccountController::class, 'createAccount'])->middleware('ipCheck')->name('api.v1.accounts.create');
     Route::delete('accounts/{id}', [AccountController::class, 'deleteAccount'])->middleware('ipCheck')->name('api.v1.accounts.delete');
+    Route::put('accounts/{id}', [AccountController::class, 'updateAccount'])->middleware('ipCheck')->name('api.v1.accounts.update');
 });
