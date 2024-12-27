a6e9757b0de59dec603e87bd39446d0c5621edde
eden-kl <wl02635565@gmail.com>
2024-12-27 11:16:42 +0800

    [add] 新增delete、修改路徑

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\v1\AccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function () {
-    Route::get('account', [AccountController::class, 'getList'])->middleware('ipCheck')->name('api.v1.account.list');
-    Route::post('account', [AccountController::class, 'createAccount'])->middleware('ipCheck')->name('api.v1.account.create');
+    Route::get('accounts', [AccountController::class, 'getList'])->middleware('ipCheck')->name('api.v1.accounts.list');
+    Route::post('accounts', [AccountController::class, 'createAccount'])->middleware('ipCheck')->name('api.v1.accounts.create');
+    Route::delete('accounts/{id}', [AccountController::class, 'deleteAccount'])->middleware('ipCheck')->name('api.v1.accounts.delete');
 });
