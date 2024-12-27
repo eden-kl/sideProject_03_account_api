9d5d1cd805697e6f30a46ea0ad984e769364d5e3
eden-kl <wl02635565@gmail.com>
2024-12-27 20:51:07 +0800

    [add] 新增get單筆route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function () {
     Route::get('accounts', [AccountController::class, 'getList'])->middleware('ipCheck')->name('api.v1.accounts.list');
+    Route::get('accounts/{id}', [AccountController::class, 'getOne'])->middleware('ipCheck')->name('api.v1.accounts.one');
     Route::post('accounts', [AccountController::class, 'createAccount'])->middleware('ipCheck')->name('api.v1.accounts.create');
     Route::delete('accounts/{id}', [AccountController::class, 'deleteAccount'])->middleware('ipCheck')->name('api.v1.accounts.delete');
     Route::put('accounts/{id}', [AccountController::class, 'updateAccount'])->middleware('ipCheck')->name('api.v1.accounts.update');
